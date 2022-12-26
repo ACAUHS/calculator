e9e2ad30c281639d88f6302a7281f395a9328371
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-25 19:33:54 -0500

    Add TypeScript to project
    Created the four basic functions of a calculator.

---
+++ logic.js
@@ -0,0 +1,41 @@
+"use strict";
+/**
+ *
+ * @param a first number
+ * @param b second number
+ * @returns Adds the two numbers together and returns the sum
+ */
+function addition(a, b) {
+    const sum = a + b;
+    return sum;
+}
+/**
+ *
+ * @param a first number
+ * @param b second number
+ * @returns Subtracts the first number from the second and returns the difference
+ */
+function subtract(a, b) {
+    const difference = a - b;
+    return difference;
+}
+/**
+ *
+ * @param a first number
+ * @param b second number
+ * @returns Multiplies the two numbers together and returns the product of the two
+ */
+function multiply(a, b) {
+    const product = a * b;
+    return product;
+}
+/**
+ *
+ * @param a first number
+ * @param b second number
+ * @returns Divides the first number by the second number and returns the quotient
+ */
+function divide(a, b) {
+    const quotient = a / b;
+    return quotient;
+}
---
+++ logic.ts
@@ -0,0 +1,44 @@
+/**
+ * 
+ * @param a first number
+ * @param b second number
+ * @returns Adds the two numbers together and returns the sum
+ */
+function addition(a:number, b:number) {
+    const sum = a + b
+    return sum
+}
+
+/**
+ * 
+ * @param a first number
+ * @param b second number
+ * @returns Subtracts the first number from the second and returns the difference
+ */
+function subtract(a:number, b:number) {
+    const difference = a - b
+    return difference
+}
+
+
+/**
+ * 
+ * @param a first number
+ * @param b second number
+ * @returns Multiplies the two numbers together and returns the product of the two
+ */
+function multiply (a:number, b:number) {
+    const product = a * b
+    return product
+}
+
+/**
+ * 
+ * @param a first number
+ * @param b second number
+ * @returns Divides the first number by the second number and returns the quotient
+ */
+function divide (a:number, b:number) {
+    const quotient = a / b
+    return quotient
+}
